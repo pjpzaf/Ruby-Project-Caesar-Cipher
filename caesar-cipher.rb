5a5d1ebe2a471403ac72a8b1b7d56b72613f3e17
pjpzaf <paolojosemari.zafra@gmail.com>
2024-03-12 16:22:32 +0800

    Added encryption code block

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'pry-byebug'
 
 def caesar_cipher(input_string, shift_number)
@@ -11,14 +12,34 @@ def caesar_cipher(input_string, shift_number)
 
   # Create array of letters in alphabets
   the_alphabet = Array.new
-  array_index_internal = 0
-  for array_index in 'a'..'z'
-    the_alphabet[array_index_internal] = array_index
-    array_index_internal += 1
+  first_loop_counter = 0
+  for first_array_index in 'a'..'z'
+    the_alphabet[first_loop_counter] = first_array_index
+    first_loop_counter += 1
   end 
 
+  # Encryption code block
   process_string = input_string.split("")
-
+  transformed_string = Array.new
+  second_loop_counter = 0
+  process_string.each do |process_string_element|
+    if the_alphabet.include?(process_string_element.downcase)
+      initial_index = the_alphabet.index(process_string_element.downcase)
+      shifted_index = (shift_number + initial_index) % 26
+    
+      if the_alphabet[initial_index] == process_string_element
+        transformed_string[second_loop_counter] = the_alphabet[shifted_index]
+        second_loop_counter += 1
+      else
+        transformed_string[second_loop_counter] = the_alphabet[shifted_index].upcase
+        second_loop_counter += 1
+      end
+    else
+      transformed_string[second_loop_counter] = process_string_element
+      second_loop_counter += 1
+    end
+  end
+  puts transformed_string.join("")
 end
 
-caesar_cipher("sample text",-4)
+caesar_cipher("SAMPLE text123231231245***%$@$",-500)
