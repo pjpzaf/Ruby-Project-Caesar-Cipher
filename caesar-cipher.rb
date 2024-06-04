0971f3dc8e401c1409b62b7669ec3f538d25ef6f
pjpzaf <paolojosemari.zafra@gmail.com>
2024-06-04 15:36:51 +0800

    Added gemfile and gemfile.lock files through bundler and applied ran rubocop to file for linting

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,45 +1,43 @@
+# frozen_string_literal: true
+
 require 'pry'
 require 'pry-byebug'
 
 def caesar_cipher(input_string, shift_number)
-
   # To check if shift_number is an integer
-  if shift_number.is_a? Integer
-  else
-    puts "Shift number must be an integer!"
+  unless shift_number.is_a? Integer
+    puts 'Shift number must be an integer!'
     exit
   end
 
   # Create array of letters in alphabets
-  the_alphabet = Array.new
+  the_alphabet = []
   first_loop_counter = 0
-  for first_array_index in 'a'..'z'
+  ('a'..'z').each do |first_array_index|
     the_alphabet[first_loop_counter] = first_array_index
     first_loop_counter += 1
-  end 
+  end
 
   # Encryption code block
-  process_string = input_string.split("")
-  transformed_string = Array.new
+  process_string = input_string.split('')
+  transformed_string = []
   second_loop_counter = 0
   process_string.each do |process_string_element|
     if the_alphabet.include?(process_string_element.downcase)
       initial_index = the_alphabet.index(process_string_element.downcase)
       shifted_index = (shift_number + initial_index) % 26
-    
-      if the_alphabet[initial_index] == process_string_element
-        transformed_string[second_loop_counter] = the_alphabet[shifted_index]
-        second_loop_counter += 1
-      else
-        transformed_string[second_loop_counter] = the_alphabet[shifted_index].upcase
-        second_loop_counter += 1
-      end
+
+      transformed_string[second_loop_counter] = if the_alphabet[initial_index] == process_string_element
+                                                  the_alphabet[shifted_index]
+                                                else
+                                                  the_alphabet[shifted_index].upcase
+                                                end
     else
       transformed_string[second_loop_counter] = process_string_element
-      second_loop_counter += 1
     end
+    second_loop_counter += 1
   end
-  puts transformed_string.join("")
+  puts transformed_string.join('')
 end
 
-caesar_cipher("SAMPLE text123231231245***%$@$",-500)
+caesar_cipher('What a string!', 5)
